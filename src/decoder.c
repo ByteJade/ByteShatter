cc5d08cd776601f7d497dfcba33efd351b5998bf
ByteJade <cuvakcernyj66@gmail.com>
2026-06-28 13:25:27 +0300

    Decoder refactoring

--- src/decoder.c
+++ src/decoder.c
@@ -7,8 +7,6 @@
 #include <stdint.h>
 #include <stdio.h>
 
-X64_instruction buf;
-
 int64_t fetch_imm8() {
     return (int64_t)(int8_t)fetch8();
 }
@@ -59,15 +57,15 @@ void decode_rm(Operand* op, uint8_t modrm) {
         op->reg = rm;
     }
 }
-void decode_regrm() {
+void decode_regrm(X64_instruction* buf) {
     uint8_t byte = fetch8();
-    decode_reg(&buf.op0, byte);
-    decode_rm(&buf.op1, byte);
+    decode_reg(&buf->op0, byte);
+    decode_rm(&buf->op1, byte);
 }
 
-void print_op(Operand* op) {
+void print_op(X64_instruction* buf, Operand* op) {
     if (op->type == REG) {
-        if (buf.size == 64) {
+        if (buf->size == 64) {
             printf("r%s ", regs[op->reg]);
         } else {
             printf("e%s ", regs[op->reg]);
@@ -98,37 +96,37 @@ void print_op(Operand* op) {
         printf("] ");
     }
 }
-void print_instr() {
-    printf("%s ", types[buf.type]);
-    if (buf.opcount > 0)
-        print_op(&buf.op0);
-    if (buf.opcount > 1)
-        print_op(&buf.op1);
+void print_instr(X64_instruction* buf) {
+    printf("%s ", types[buf->type]);
+    if (buf->opcount > 0)
+        print_op(buf, &buf->op0);
+    if (buf->opcount > 1)
+        print_op(buf, &buf->op1);
     printf("\n");
 }
-int decode_instruction() {
+int decode_instr(X64_instruction* buf) {
     int ret = 0;
     int reverse = 0;
     uint8_t rex = 0;
     uint8_t byte = fetch8();
-    buf.size = 32;
+    buf->size = 32;
     if (byte >> 4 == 0x4) {
         rex = byte & 0xF;
         byte = fetch8();
     } else if (byte == 0x66) {
-        buf.size = 16;
+        buf->size = 16;
         byte = fetch8();
     }
     switch (byte) {
         case 0x31:
-            buf.opcount = 2;
-            buf.type = XOR;
-            decode_regrm();
+            buf->opcount = 2;
+            buf->type = XOR;
+            decode_regrm(buf);
             break;
         case 0x3B:
-            buf.opcount = 2;
-            buf.type = CMP;
-            decode_regrm();
+            buf->opcount = 2;
+            buf->type = CMP;
+            decode_regrm(buf);
             break;
         case 0x50:
         case 0x51:
@@ -138,11 +136,11 @@ int decode_instruction() {
         case 0x55:
         case 0x56:
         case 0x57:
-            buf.size = 64;
-            buf.opcount = 1;
-            buf.type = PUSH;
-            buf.op0.type = REG;
-            buf.op0.reg = byte - 0x50;
+            buf->size = 64;
+            buf->opcount = 1;
+            buf->type = PUSH;
+            buf->op0.type = REG;
+            buf->op0.reg = byte - 0x50;
             break;
         case 0x58:
         case 0x59:
@@ -152,91 +150,91 @@ int decode_instruction() {
         case 0x5D:
         case 0x5E:
         case 0x5F:
-            buf.size = 64;
-            buf.opcount = 1;
-            buf.type = POP;
-            buf.op0.type = REG;
-            buf.op0.reg = byte - 0x58;
+            buf->size = 64;
+            buf->opcount = 1;
+            buf->type = POP;
+            buf->op0.type = REG;
+            buf->op0.reg = byte - 0x58;
             break;
         case 0x74:
-            buf.opcount = 1;
-            buf.type = JE;
-            buf.op0.type = IMM;
-            buf.op0.imm = fetch_imm8();
+            buf->opcount = 1;
+            buf->type = JE;
+            buf->op0.type = IMM;
+            buf->op0.imm = fetch_imm8();
             ret = JE;
             break;
         case 0x83: {
             reverse = 1;
-            buf.opcount = 2;
+            buf->opcount = 2;
             uint8_t modrm = fetch8();
-            buf.op0.type = IMM;
-            buf.op0.imm = fetch_imm8();
-            decode_rm(&buf.op1, modrm);
+            buf->op0.type = IMM;
+            buf->op0.imm = fetch_imm8();
+            decode_rm(&buf->op1, modrm);
             switch ((modrm >> 3)&7) {
-                case 0: buf.type = ADD; break;
+                case 0: buf->type = ADD; break;
                 case 1: break; // or
                 case 2: break; // adc
                 case 3: break; // sbb
-                case 4: buf.type = AND; break;
-                case 5: buf.type = SUB; break;
-                case 6: buf.type = XOR; break;
+                case 4: buf->type = AND; break;
+                case 5: buf->type = SUB; break;
+                case 6: buf->type = XOR; break;
                 case 7: break; // cmp
             }
         } break;
         case 0x85:
-            buf.opcount = 2;
-            buf.type = TST;
-            decode_regrm();
+            buf->opcount = 2;
+            buf->type = TST;
+            decode_regrm(buf);
             break;
         case 0x89:
             reverse = 1;
-            buf.opcount = 2;
-            buf.type = MOV;
-            decode_regrm();
+            buf->opcount = 2;
+            buf->type = MOV;
+            decode_regrm(buf);
             break;
         case 0x8B:
-            buf.opcount = 2;
-            buf.type = MOV;
-            decode_regrm();
+            buf->opcount = 2;
+            buf->type = MOV;
+            decode_regrm(buf);
             break;
         case 0x8D:
-            buf.opcount = 2;
-            buf.type = LEA;
-            decode_regrm();
+            buf->opcount = 2;
+            buf->type = LEA;
+            decode_regrm(buf);
             break;
         case 0xB8:
-            buf.opcount = 2;
-            buf.type = MOV;
-            buf.op0.type = REG;
-            buf.op0.reg = RAX;
-            buf.op1.type = IMM;
-            buf.op1.imm = fetch32();
+            buf->opcount = 2;
+            buf->type = MOV;
+            buf->op0.type = REG;
+            buf->op0.reg = RAX;
+            buf->op1.type = IMM;
+            buf->op1.imm = fetch32();
             break;
         case 0xE8:
-            buf.opcount = 1;
-            buf.type = CALL;
-            buf.op0.type = IMM;
-            buf.op0.imm = fetch32();
+            buf->opcount = 1;
+            buf->type = CALL;
+            buf->op0.type = IMM;
+            buf->op0.imm = fetch32();
             break;
         case 0xEb:
-            buf.opcount = 1;
-            buf.type = JMP;
+            buf->opcount = 1;
+            buf->type = JMP;
             ret = JMP;
-            buf.op0.type = IMM;
-            buf.op0.imm = fetch8();
+            buf->op0.type = IMM;
+            buf->op0.imm = fetch8();
             break;
         case 0xC7:
             reverse = 1;
-            buf.opcount = 2;
-            buf.type = MOV;
-            decode_rm(&buf.op1, fetch8());
-            buf.op0.type = IMM;
-            buf.op0.imm = fetch32();
+            buf->opcount = 2;
+            buf->type = MOV;
+            decode_rm(&buf->op1, fetch8());
+            buf->op0.type = IMM;
+            buf->op0.imm = fetch32();
             break;
         case 0xC3:
         case 0xF4: // actualy hlt
-            buf.opcount = 0;
-            buf.type = RET;
+            buf->opcount = 0;
+            buf->type = RET;
             ret = RET;
             break;
         case 0xf3: // currently only endbr64 is used
@@ -244,45 +242,43 @@ int decode_instruction() {
             fetch16(); fetch8(); // just skip
             return 0;
         case 0xff: {
-            buf.size = 64;
+            buf->size = 64;
             reverse = 1;
-            buf.opcount = 1;
+            buf->opcount = 1;
             uint8_t modrm = fetch8();
-            decode_rm(&buf.op1, modrm);
+            decode_rm(&buf->op1, modrm);
             switch ((modrm >> 3)&7) {
                 case 0:
-                    buf.type = ADD;
-                    buf.op0.type = IMM;
-                    buf.op0.imm = 1;
+                    buf->type = ADD;
+                    buf->op0.type = IMM;
+                    buf->op0.imm = 1;
                     break; // inc
                 case 1:
-                    buf.type = SUB;
-                    buf.op0.type = IMM;
-                    buf.op0.imm = 1;
+                    buf->type = SUB;
+                    buf->op0.type = IMM;
+                    buf->op0.imm = 1;
                     break; // dec
                 case 2:
-                case 3: buf.type = CALL; break; // call
+                case 3: buf->type = CALL; break; // call
                 case 4:
-                case 5: buf.type = JMP; ret = JMP; break; // jmps
-                case 6: buf.type = PUSH; break; // push
+                case 5: buf->type = JMP; ret = JMP; break; // jmps
+                case 6: buf->type = PUSH; break; // push
             }
         } break;
         default:
             panic("DECODER::UNKNOWN_SYMBOL: %X", byte);
     }
     if (rex) {
-        if (rex&8) buf.size = 64;
-        if (rex&4) buf.op0.reg += 8;
-        if (rex&2) buf.op1.idx += 8;
-        if (rex&1) buf.op1.reg += 8;
+        if (rex&8) buf->size = 64;
+        if (rex&4) buf->op0.reg += 8;
+        if (rex&2) buf->op1.idx += 8;
+        if (rex&1) buf->op1.reg += 8;
     }
     if (reverse) {
-        Operand tmp = buf.op0;
-        buf.op0 = buf.op1;
-        buf.op1 = tmp;
+        Operand tmp = buf->op0;
+        buf->op0 = buf->op1;
+        buf->op1 = tmp;
     }
-    print_instr();
-    encode(&buf);
     return ret;
 }
 
@@ -292,7 +288,10 @@ void decode(uint32_t gp) {
     cache_block_start();
     while (1) {
         cache_block_point();
-        uint8_t jump_type = decode_instruction();
+        X64_instruction buf;
+        uint8_t jump_type = decode_instr(&buf);
+        print_instr(&buf);
+        encode(&buf);
         if (jump_type == RET || jump_type == JMP) break;
     }
     cache_block_end();
