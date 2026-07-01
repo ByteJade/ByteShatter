0eb0f18aa2770ae0f7018890d6bf23ae38db1999
ByteJade <cuvakcernyj66@gmail.com>
2026-07-01 14:19:08 +0300

    Better address emit

--- src/encoder.c
+++ src/encoder.c
@@ -63,9 +63,7 @@ void emit_address_decode(Operand* op) {
         }
     }else {
         if (t&IMM) {
-            if (op->imm >= 0)
-                emit32(_construct_r_r_imm(SF|ADD_IMM, SC1, op->reg, op->imm));
-            else emit32(_construct_r_r_imm(SF|SUB_IMM, SC1, op->reg, -op->imm));
+            emit_add_signed(SC1, op->reg, op->imm);
         } else {
             emit32(_construct_r_r_imm(SF|ADD_IMM, SC1, op->reg, 0));
         }
