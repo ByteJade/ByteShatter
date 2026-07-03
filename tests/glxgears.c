cab12c74d4ff6fef4e814abd13de648a6e3ddf62
ByteJade <cuvakcernyj66@gmail.com>
2026-07-03 20:41:02 +0300

    add tests

--- tests/glxgears.c
+++ tests/glxgears.c
@@ -3,8 +3,9 @@
 
 int main(int argc, char *argv[])
 {
-    printf("arg: %s\n", argv[argc-1]);
-    if (strcmp(argv[argc-1], "-display") == 0) {
+    const char* test = "-display";
+    printf("arg: %s; test: %s\n", argv[argc-1], test);
+    if (strcmp(argv[argc-1], test) == 0) {
         printf("DISPLAY\n");
     } else {
         printf("NOT DISPLAY\n");
