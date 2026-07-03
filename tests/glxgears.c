90998a06f0ae5b47bd67d6a1316755d542c49d26
ByteJade <cuvakcernyj66@gmail.com>
2026-07-03 20:52:07 +0300

    add tests

--- tests/glxgears.c
+++ tests/glxgears.c
@@ -5,11 +5,15 @@ int main(int argc, char *argv[])
 {
     int i;
     for (i = 1; i < argc; i++) {
-        if (i < argc-1 && strcmp(argv[i], "-framerate") == 0) {
+        if (strcmp(argv[i], "-display") == 0) {
+            printf("DISPLAY\n");
+        }
+        else if (i < argc-1 && strcmp(argv[i], "-framerate") == 0) {
             printf("FRAMERATE\n");
         }
         else {
-            printf("NOT");
+            printf("NOTHING\n");
+            return -1;
         }
     }
     return 0;
