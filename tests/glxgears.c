f3633ce7ea62f6611eceb09063839f4f22ae212d
ByteJade <cuvakcernyj66@gmail.com>
2026-07-03 19:37:20 +0300

    add tests

--- tests/glxgears.c
+++ tests/glxgears.c
@@ -1,18 +1,6 @@
 #include <stdio.h>
 #include <string.h>
 
-static void usage()
-{
-    printf("Usage:\n");
-    printf("  -display <displayname>  set the display to run on\n");
-    printf("  -stereo                 run in stereo mode\n");
-    printf("  -samples N              run in multisample mode with at least N samples\n");
-    printf("  -fullscreen             run in fullscreen mode\n");
-    printf("  -info                   display OpenGL renderer info\n");
-    printf("  -geometry WxH+X+Y       window geometry\n");
-    printf("  -framerate N            limit framerate to N frams/sec\n");
-}
-
 int main(int argc, char *argv[])
 {
     if (strcmp(argv[argc-1], "-display") == 0) {
