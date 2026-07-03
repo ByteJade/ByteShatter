6dac9de1994259ad8fd0e1b925013164e4721000
ByteJade <cuvakcernyj66@gmail.com>
2026-07-03 20:28:08 +0300

    add tests

--- tests/glxgears.c
+++ tests/glxgears.c
@@ -6,6 +6,8 @@ int main(int argc, char *argv[])
     printf("arg: %s\n", argv[argc-1]);
     if (strcmp(argv[argc-1], "-display") == 0) {
         printf("DISPLAY\n");
+    } else {
+        printf("NOT DISPLAY\n");
     }
     return 0;
 }
