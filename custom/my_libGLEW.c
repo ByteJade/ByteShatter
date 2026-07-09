8e2348e60ce9f42680c0e3d58d4091f1270991c1
ByteJade <cuvakcernyj66@gmail.com>
2026-07-09 15:56:15 +0300

    Add new wrappers

--- custom/my_libGLEW.c
+++ custom/my_libGLEW.c
@@ -1,9 +1,1 @@
-#include <GL/glew.h>
 #include "wrapper.h"
-
-WRAP(GLenum, glewInit, void);
-WRAP(GLboolean, glewIsSupported, const char *name);
-WRAP(int, glewGetExtension, const char *name);
-WRAP(int, glewGetError, void);
-WRAP(const char*, glewGetString, GLenum name);
-WRAP(void, glewExperimental, void);
