902aacc9346818e4d72b3ee763d00062100d3c54
ByteJade <cuvakcernyj66@gmail.com>
2026-07-12 21:43:28 +0300

    satisfy gcc

--- src/patcher.c
+++ src/patcher.c
@@ -39,7 +39,7 @@ void print_native_cpu(void) {
     print("Flags: N%x Z%x C%x V%x", N, Z, C, V);
     #endif
 }
-void brk_handler(int, siginfo_t* info, void* ucontext) {
+void brk_handler(int sig, siginfo_t* info, void* ucontext) {
     ucontext_t* ctx = (ucontext_t*)ucontext;
     sc = (struct sigcontext*)&ctx->uc_mcontext;
     #if defined(__aarch64__) || defined(_M_ARM64)
@@ -107,7 +107,7 @@ void brk_handler(int, siginfo_t* info, void* ucontext) {
     }
     #endif
 }
-void segv_handler(int, siginfo_t* info, void* ucontext) {
+void segv_handler(int sig, siginfo_t* info, void* ucontext) {
     ucontext_t* ctx = (ucontext_t*)ucontext;
     struct sigcontext* sc = (struct sigcontext*)&ctx->uc_mcontext;
     print_cpu();
