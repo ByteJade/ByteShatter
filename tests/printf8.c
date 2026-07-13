13aef5af87a5fdbcd251076ab79e4c5820ea4c5b
ByteJade <cuvakcernyj66@gmail.com>
2026-07-13 13:57:52 +0300

    test

--- tests/printf8.c
+++ tests/printf8.c
@@ -9,6 +9,6 @@ int main() {
     int x2 = 1;
     int x3 = 2147483647;
     int x4 = -2147483648;
-    printf("%s %x %s %x %s %x %s %x\n",
+    printf("%s %i %s %i %s %i %s %i\n",
                 s1, x1, s2, x2, s3, x3, s4, x4);
 }
