47662f91700253e941edf997fe913c813331de02
ByteJade <cuvakcernyj66@gmail.com>
2026-07-13 20:29:13 +0300

    Attempt to switch to assembler

--- custom/my_libX11.s
+++ custom/my_libX11.s
@@ -7,14 +7,14 @@ WRAP_BIG_FUNC XSetStandardProperties
 WRAP_BIG_FUNC XChangeProperty
 WRAP_BIG_FUNC XCreateWindow
 
-WRAP XInternAtom
-WRAP DefaultScreen
-WRAP RootWindow
-WRAP XOpenDisplay
-WRAP XCloseDisplay
-WRAP XFree
-WRAP XMapWindow
-WRAP XDestroyWindow
-WRAP XSetNormalHints
-WRAP XParseGeometry
-WRAP XCreateColormap
+WRAP_FUNC XInternAtom
+WRAP_FUNC DefaultScreen
+WRAP_FUNC RootWindow
+WRAP_FUNC XOpenDisplay
+WRAP_FUNC XCloseDisplay
+WRAP_FUNC XFree
+WRAP_FUNC XMapWindow
+WRAP_FUNC XDestroyWindow
+WRAP_FUNC XSetNormalHints
+WRAP_FUNC XParseGeometry
+WRAP_FUNC XCreateColormap
