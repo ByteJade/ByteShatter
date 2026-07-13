d0818635d3f2d91c8550a02b114ca16228fffa40
ByteJade <cuvakcernyj66@gmail.com>
2026-07-13 14:22:46 +0300

    test

--- tests/printf8.c
+++ tests/printf8.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
 int main() {
-    const char* s1 = "neg";
-    const char* s2 = "pos";
-    const char* s3 = "max";
-    const char* s4 = "min";
-    int x1 = -1;
-    int x2 = 1;
-    int x3 = 2147483647;
-    int x4 = -2147483648;
+    const char* s1 = "max";
+    const char* s2 = "min";
+    const char* s3 = "neg";
+    const char* s4 = "pos";
+    int x1 = 2147483647;
+    int x2 = -2147483648;
+    int x3 = -1;
+    int x4 = 1;
     printf("%s %i %s %i %s %i %s %i\n",
                 s1, x1, s2, x2, s3, x3, s4, x4);
 }
