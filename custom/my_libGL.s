f4f7c1b2a98bb86a136176e80f6487a23262c2bb
ByteJade <cuvakcernyj66@gmail.com>
2026-07-13 22:46:44 +0300

    Attempt to switch to assembler

--- custom/my_libGL.s
+++ custom/my_libGL.s
@@ -2,12 +2,12 @@
 
 .section .text
 
-WRAP_FUNC(glGetString)
+WRAP_FUNC glGetString
 
-WRAP_FUNC(glXMakeCurrent)
-WRAP_FUNC(glXGetProcAddressARB)
-WRAP_FUNC(glXDestroyContext)
-WRAP_FUNC(glXCreateContext)
-WRAP_FUNC(glXQueryDrawable)
-WRAP_FUNC(glXChooseVisual)
-WRAP_FUNC(glXQueryExtensionsString)
+WRAP_FUNC glXMakeCurrent
+WRAP_FUNC glXGetProcAddressARB
+WRAP_FUNC glXDestroyContext
+WRAP_FUNC glXCreateContext
+WRAP_FUNC glXQueryDrawable
+WRAP_FUNC glXChooseVisual
+WRAP_FUNC glXQueryExtensionsString
