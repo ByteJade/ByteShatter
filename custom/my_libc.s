35b8c05b4e30e42eed12fd3d80c6bd0460b55317
ByteJade <cuvakcernyj66@gmail.com>
2026-07-13 13:02:10 +0300

    Attempt to switch to assembler

---
+++ custom/my_libc.s
@@ -0,0 +1,36 @@
+.section .text
+.global my___libc_start_main
+.extern printf
+.global my_printf
+
+.macro WRAP_FUNC alias
+    .global my_\alias
+    .extern \alias
+    my_\alias:
+        b \alias
+.endm
+
+// just call main and return
+my___libc_start_main:
+    stp x29, x30, [sp, #-16]!
+    mov x7, x0
+    mov x0, x1
+    mov x1, x2
+    bl x7
+    ldp x29, x30, [sp], #16
+    ret
+
+my_printf:
+    stp x29, x30, [sp, #-16]!
+    ldp x6, x7, [x28]
+    bl printf
+    ldp x29, x30, [sp], #16
+    ret
+
+WRAP_FUNC exit
+WRAP_FUNC puts
+WRAP_FUNC malloc
+WRAP_FUNC free
+WRAP_FUNC strlen
+WRAP_FUNC strstr
+WRAP_FUNC getenv
