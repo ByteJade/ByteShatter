2298386126b0827f1e69a459fa767b5eff2ff6d7
ByteJade <cuvakcernyj66@gmail.com>
2026-07-14 23:32:30 +0300

    Add aarch64 decoding

--- src/arm64printer.c
+++ src/arm64printer.c
@@ -42,21 +42,20 @@ void sprint_x_x_imm(char* out, char* name, uint32_t buf) {
 void sprint_x_mem(char* out, char* name, uint32_t buf) {
     uint8_t rd = buf & 0x1F;
     uint8_t rn = (buf >> 5) & 0x1F;
-    uint16_t imm = (buf >> 12) & 0xFFF;
+    uint16_t imm = (buf >> 10) & 0xFFF;
     int W = (buf >> 10) & 1;  // Write-back?
     int P = (buf >> 11) & 1;  // Pre-indexed?
-    int U = (buf >> 23) & 1;  // Add or subtract?
+    int U = (buf >> 24) & 1;  // Add or subtract?
     int32_t offset = imm;
     if (!U) offset = -offset;
     char size = 'W';
     if (buf&(1<<30)) size = 'X';
     out += sprintf(out, "%s %c%i, [X%i",
         name, size, rd, rn);
-    if (imm) out += sprintf(out, ", #%i", offset);
     if (P) {
         // Pre-indexed: [Xn, #offset]
         if (W && offset != 0) {
-            out += sprintf(out, ", #%d]!", offset);
+            out += sprintf(out, ", #%x]!", offset);
         } else {
             out += sprintf(out, "]");
         }
@@ -64,7 +63,7 @@ void sprint_x_mem(char* out, char* name, uint32_t buf) {
         // Post-indexed: [Xn], #offset
         out += sprintf(out, "]");
         if (W && offset != 0) {
-            out += sprintf(out, ", #%d", offset);
+            out += sprintf(out, ", #%x", offset);
         }
     }
 }
