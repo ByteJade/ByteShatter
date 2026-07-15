073fb6b9efc92e3e6bfe5395c604444bac262cc8
ByteJade <cuvakcernyj66@gmail.com>
2026-07-15 15:28:27 +0300

    Style update

--- src/arm64printer.c
+++ src/arm64printer.c
@@ -13,8 +13,8 @@ int comp(const char* cond, uint64_t opcode) {
     }
     return ((opcode&mask) == check);
 }
-void sprint_x_x_x(char* out, char* name, uint32_t buf) {
-    out += sprintf(out, "\033[35m");
+void sprint_x_x_x(char** out, char* name, uint32_t buf) {
+    char* ptr = *out;
     uint8_t rd = (buf >> 0) & 0x1F;
     uint8_t rn = (buf >> 5) & 0x1F;
     uint8_t rm = (buf >> 16) & 0x1F;
@@ -23,13 +23,13 @@ void sprint_x_x_x(char* out, char* name, uint32_t buf) {
     if (buf&(1<<31)) size = 'X';
     char sign = ' ';
     if (buf&(1<<29)) sign = 'S';
-    sprintf(out, "%s%c %c%i, %c%i, %c%i",
+    ptr += sprintf(ptr, "%s%c %c%i, %c%i, %c%i",
         name, sign, size, rd, size, rn, size, rm);
-    if (shift) out += sprintf(out, "{%i}", shift);
-    out += sprintf(out, "\033[0m");
+    if (shift) ptr += sprintf(ptr, "{%i}", shift);
+    *out = ptr;
 }
-void sprint_x_x_imm(char* out, char* name, uint32_t buf) {
-    out += sprintf(out, "\033[35m");
+void sprint_x_x_imm(char** out, char* name, uint32_t buf) {
+    char* ptr = *out;
     uint8_t rd = buf & 0x1F;
     uint8_t rn = (buf >> 5) & 0x1F;
     uint16_t imm = (buf >> 10) & 0xFFF;
@@ -38,13 +38,13 @@ void sprint_x_x_imm(char* out, char* name, uint32_t buf) {
     if (buf&(1<<31)) size = 'X';
     char sign = ' ';
     if (buf&(1<<29)) sign = 'S';
-    out += sprintf(out, "%s%c %c%i, %c%i, #%x",
+    ptr += sprintf(ptr, "%s%c %c%i, %c%i, #%x",
         name, sign,  size, rd, size, rn, imm);
-    if (shift) out += sprintf(out, "{%i}", shift);
-    out += sprintf(out, "\033[0m");
+    if (shift) ptr += sprintf(ptr, "{%i}", shift);
+    *out = ptr;
 }
-void sprint_x_mem(char* out, char* name, uint32_t buf) {
-    out += sprintf(out, "\033[35m");
+void sprint_x_mem(char** out, char* name, uint32_t buf) {
+    char* ptr = *out;
     uint8_t rd = buf & 0x1F;
     uint8_t rn = (buf >> 5) & 0x1F;
     int16_t imm = (buf >> 12) & 0x1FF;
@@ -55,62 +55,63 @@ void sprint_x_mem(char* out, char* name, uint32_t buf) {
     }
     char size = 'W';
     if (buf&(1<<30)) size = 'X';
-    out += sprintf(out, "%s %c%i, [X%i",
+    ptr += sprintf(ptr, "%s %c%i, [X%i",
         name, size, rd, rn);
     if (P) {
         // Pre-indexed: [Xn, #offset]
         if (W && imm) {
-            out += sprintf(out, ", #%i]!", imm);
+            ptr += sprintf(ptr, ", #%i]!", imm);
         } else {
-            out += sprintf(out, "]");
+            ptr += sprintf(ptr, "]");
         }
     } else {
         // Post-indexed: [Xn], #offset
-        out += sprintf(out, "]");
+        ptr += sprintf(ptr, "]");
         if (W && imm) {
         }
     }
-    out += sprintf(out, "\033[0m");
+    *out = ptr;
 }
-void sprint_x_imm(char* out, char* name, uint32_t buf) {
-    out += sprintf(out, "\033[35m");
+void sprint_x_imm(char** out, char* name, uint32_t buf) {
+    char* ptr = *out;
     uint8_t rd = buf & 0x1F;
     uint16_t imm = (buf >> 5) & 0xFFFF;
     uint8_t shift = (buf >> 21) & 0x3;
     char size = 'W';
     if (buf&(1<<31)) size = 'X';
-    out += sprintf(out, "%s %c%i, #%i", name, size, rd, imm);
-    if (shift) out += sprintf(out, "{%i}", shift);
-    out += sprintf(out, "\033[0m");
+    ptr += sprintf(ptr, "%s %c%i, #%i", name, size, rd, imm);
+    if (shift) ptr += sprintf(ptr, "{%i}", shift);
+    *out = ptr;
 }
 void sprint_arm(char* out, uint32_t buf) {
+    out += sprintf(out, "\033[35m");
     if (comp("-0-01011------------------------", buf))
-        {sprint_x_x_x(out, "add", buf); return;}
-    if (comp("-1-01011------------------------", buf))
-        {sprint_x_x_x(out, "sub", buf); return;}
-    if (comp("-0-10001------------------------", buf))
-        {sprint_x_x_imm(out, "add", buf); return;}
-    if (comp("-1-10001------------------------", buf))
-        {sprint_x_x_imm(out, "sub", buf); return;}
-    if (comp("-1-01010------------------------", buf))
-        {sprint_x_x_x(out, "eor", buf); return;}
-    if (comp("1-11100-01----------------------", buf))
-        {sprint_x_mem(out, "ldr", buf); return;}
-    if (comp("1-11100-00----------------------", buf))
-        {sprint_x_mem(out, "str", buf); return;}
-    if (comp("11010100001---------------------", buf))
-        {sprintf(out, "brk %x", (buf>>5)&0xFFFF); return;}
-
-    if (comp("-101001010----------------------", buf))
-        {sprint_x_imm(out, "movz", buf); return;}
+        sprint_x_x_x(&out, "add", buf);
+    else if (comp("-1-01011------------------------", buf))
+        sprint_x_x_x(&out, "sub", buf);
+    else if (comp("-0-10001------------------------", buf))
+        sprint_x_x_imm(&out, "add", buf);
+    else if (comp("-1-10001------------------------", buf))
+        sprint_x_x_imm(&out, "sub", buf);
+    else if (comp("-1-01010------------------------", buf))
+        sprint_x_x_x(&out, "eor", buf);
+    else if (comp("1-11100-01----------------------", buf))
+        sprint_x_mem(&out, "ldr", buf);
+    else if (comp("1-11100-00----------------------", buf))
+        sprint_x_mem(&out, "str", buf);
+    else if (comp("-101001010----------------------", buf))
+        sprint_x_imm(&out, "movz", buf);
         
-    if (comp("1101011001011111----------------", buf))
-        {sprintf(out, "ret X%i", (buf>>5)&0x1F); return;}
-    if (comp("1101011000111111000000-----00000", buf))
-        {sprintf(out, "blr X%i", (buf>>5)&0x1F); return;}
-    if (comp("0--10000------------------------", buf))
-        {sprintf(out, "adr X%i, #%x", buf&0x1F, ((buf >> 29) & 0x3) | (((buf >> 5) & 0x7FFFF) << 2)); return;}
-    if (comp("1--10000------------------------", buf))
-        {sprintf(out, "adrp X%i, #%x", buf&0x1F, ((buf >> 29) & 0x3) | (((buf >> 5) & 0x7FFFF) << 2)); return;}
-    sprintf(out, "und");
+    else if (comp("11010100001---------------------", buf))
+        out += sprintf(out, "brk %x", (buf>>5)&0xFFFF);
+    else if (comp("1101011001011111----------------", buf))
+        out += sprintf(out, "ret X%i", (buf>>5)&0x1F);
+    else if (comp("1101011000111111000000-----00000", buf))
+        out += sprintf(out, "blr X%i", (buf>>5)&0x1F);
+    else if (comp("0--10000------------------------", buf))
+        out += sprintf(out, "adr X%i, #%x", buf&0x1F, ((buf >> 29) & 0x3) | (((buf >> 5) & 0x7FFFF) << 2));
+    else if (comp("1--10000------------------------", buf))
+        out += sprintf(out, "adrp X%i, #%x", buf&0x1F, ((buf >> 29) & 0x3) | (((buf >> 5) & 0x7FFFF) << 2));
+    else out += sprintf(out, "und");
+    out += sprintf(out, "\033[0m");
 }
