4c6252425918faf5b52b4743b32823cb54030bbd
ByteJade <cuvakcernyj66@gmail.com>
2026-07-20 12:29:02 +0300

    test

--- tests/glxgears.c
+++ tests/glxgears.c
@@ -116,6 +116,13 @@ draw(void)
    glClearColor(1.0, 1.0, 1.0, 1.0);
    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
+   glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    glOrtho(-2.0, 2.0, -2.0, 2.0, -2.0, 2.0);
+
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+
    glPushMatrix();
    glCallList(gear1);
    glPopMatrix();
