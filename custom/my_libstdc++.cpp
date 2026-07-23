2dd2efbfacff25d35d2bbf847645dda06e1cbb9c
ByteJade <cuvakcernyj66@gmail.com>
2026-07-23 21:37:38 +0300

    add libstdc++

--- custom/my_libstdc++.cpp
+++ custom/my_libstdc++.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
 
 extern "C" {
-    extern std::ostream my__ZSt4cout;
-    
+    std::ostream my__ZSt4cout(std::cout.rdbuf());
+
     void my__ZStlsISt11char_traitsIcEERSt13basic_ostreamIcT_ES5_PKc(std::ostream& os_ptr, const char* str) {
         os_ptr << str;
     }
