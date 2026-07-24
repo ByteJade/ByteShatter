d032a41db751f81bc45e7ee4467fe1b39625e1da
ByteJade <cuvakcernyj66@gmail.com>
2026-07-24 17:29:58 +0300

    Better loader

--- src/loader.c
+++ src/loader.c
@@ -162,7 +162,7 @@ void reloc_rela(ExeMeta* exe, Elf64_Rela* rela, int size) {
                 const char* symname = elf->strtab + sym->st_name;
                 void *sym_addr = NULL;
                 if (exe->native) sym_addr = get_native_symbol(symname);
-                if (sym_addr == NULL) sym_addr = get_wrapped_symbol(symname);
+                else sym_addr = get_wrapped_symbol(symname);
     
                 if (sym_addr) {
                     *patch = (Elf64_Addr)sym_addr;
@@ -182,7 +182,7 @@ void reloc_rela(ExeMeta* exe, Elf64_Rela* rela, int size) {
                 //    sym->st_size, sym_name);
                 void *sym_addr = NULL;
                 if (exe->native) sym_addr = get_native_symbol(symname);
-                if (sym_addr == NULL) sym_addr = get_wrapped_symbol(symname);
+                else sym_addr = get_wrapped_symbol(symname);
                 //printf("R_X86_64_COPY: copying %p %p %lx\n",
                 //    (uint64_t*)patch, sym_addr, *(uint64_t*)sym_addr);
                 size_t size = sym->st_size;
