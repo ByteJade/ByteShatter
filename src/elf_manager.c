7522d1953a5581967f35d1afec0dc37ac4b4f5af
ByteJade <cuvakcernyj66@gmail.com>
2026-08-07 22:31:20 +0300

    Elf update

--- src/elf_manager.c
+++ src/elf_manager.c
@@ -80,49 +80,64 @@ char* get_symbol_wrapped(const char* symbol) {
     }
     return NULL;
 }
-void patch_library_got(const char* symbol, void* new_got) {
-    struct link_map *map = NULL;
-    for (int i = 0; i < lib_count; i++) {
-        Lib* lib = libs + i;
-        dlinfo(lib->object, RTLD_DI_LINKMAP, &map);
-        Elf64_Addr base = map->l_addr;
-        Elf64_Dyn* dyn = map->l_ld;
-
-        const char* strtab = NULL;
-        Elf64_Sym* symtab = NULL;
-        Elf64_Rela *rela = NULL;
-        size_t rela_size = 0;
-        for (; dyn->d_tag != DT_NULL; ++dyn) {
-            switch (dyn->d_tag) {
-                case DT_STRTAB:
-                    strtab = (const char*)dyn->d_un.d_ptr;
-                    break;
-                case DT_SYMTAB:
-                    symtab = (Elf64_Sym*)dyn->d_un.d_ptr;
-                    break;
-                case DT_RELA:
-                    rela = (Elf64_Rela*)dyn->d_un.d_ptr;
-                    break;
-                case DT_RELASZ:
-                    rela_size = dyn->d_un.d_val;
-                    break;
-            }
+typedef struct {
+    const char* symbol;
+    void* new_got;
+} Search_data;
+static int patch_library(struct dl_phdr_info* info, size_t size, void* data) {
+    Search_data* search = data;
+    Elf64_Addr base = info->dlpi_addr;
+    Elf64_Dyn* dyn;
+    for (int i = 0; i < info->dlpi_phnum; i++) {
+        const Elf64_Phdr* phdr = &info->dlpi_phdr[i];
+        
+        if (phdr->p_type == PT_DYNAMIC) {
+            dyn = (Elf64_Dyn*)(info->dlpi_addr + phdr->p_vaddr);
+            success("Found .dynamic at 0x%lx", dyn);
+            return 1;
         }
-        if (!(strtab && symtab && rela)) return;
-        size_t count = rela_size / sizeof(Elf64_Rela);
-        for (size_t i = 0; i < count; i++) {
-            uint32_t idx = ELF64_R_SYM(rela[i].r_info);
-            uint32_t type = ELF64_R_TYPE(rela[i].r_info);
-            Elf64_Addr* patch = (Elf64_Addr*)(base + rela[i].r_offset);
-            if (type == R_X86_64_JUMP_SLOT || type == R_X86_64_GLOB_DAT) {
-                const char* name = strtab + symtab[idx].st_name;
-                if (strcmp(name, symbol) == 0) {
-                    print("patch %s", name);
-                    void* page_start = (void*)(((uint64_t)patch) & ~(0x1000 - 1));
-                    mprotect(page_start, 0x1000, PROT_READ | PROT_WRITE);
-                    *patch = (Elf64_Addr)new_got;
-                }
+    }
+
+    const char* strtab = NULL;
+    Elf64_Sym* symtab = NULL;
+    Elf64_Rela *rela = NULL;
+    size_t rela_size = 0;
+    for (; dyn->d_tag != DT_NULL; ++dyn) {
+        switch (dyn->d_tag) {
+            case DT_STRTAB:
+                strtab = (const char*)dyn->d_un.d_ptr;
+                break;
+            case DT_SYMTAB:
+                symtab = (Elf64_Sym*)dyn->d_un.d_ptr;
+                break;
+            case DT_RELA:
+                rela = (Elf64_Rela*)dyn->d_un.d_ptr;
+                break;
+            case DT_RELASZ:
+                rela_size = dyn->d_un.d_val;
+                break;
+        }
+    }
+    if (!(strtab && symtab && rela)) return 0;
+    size_t count = rela_size / sizeof(Elf64_Rela);
+    for (size_t i = 0; i < count; i++) {
+        uint32_t idx = ELF64_R_SYM(rela[i].r_info);
+        uint32_t type = ELF64_R_TYPE(rela[i].r_info);
+        Elf64_Addr* patch = (Elf64_Addr*)(base + rela[i].r_offset);
+        if (type == R_X86_64_JUMP_SLOT || type == R_X86_64_GLOB_DAT ||
+            type == R_AARCH64_JUMP_SLOT || type == R_AARCH64_GLOB_DAT) {
+            const char* name = strtab + symtab[idx].st_name;
+            if (strcmp(name, search->symbol) == 0) {
+                print("patch %s", name);
+                void* page_start = (void*)(((uint64_t)patch) & ~(0x1000 - 1));
+                mprotect(page_start, 0x1000, PROT_READ | PROT_WRITE);
+                *patch = (Elf64_Addr)search->new_got;
             }
         }
     }
+    return 0;
+}
+void patch_library_got(const char* symbol, void* new_got) {
+    Search_data data = {symbol, new_got};
+    dl_iterate_phdr(patch_library, &data);
 }
