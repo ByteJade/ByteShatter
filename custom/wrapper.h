378c06c82d0cd46d330cf8dbef03605f4ed953da
ByteJade <cuvakcernyj66@gmail.com>
2026-08-07 15:34:20 +0300

    switch wrappers to C

--- custom/wrapper.h
+++ custom/wrapper.h
@@ -7,31 +7,32 @@
 #define WRAP_FUNC(func) \
     void my_##func() { \
         asm volatile( \
-            "mov x27, x30\n" \
+            "stp x28, x30, [sp, #-16]!\n" \
             "bl " #func "\n" \
-            "mov x30, x27\n" \
+            "ldp x28, x30, [sp], #16\n" \
             "mov x9, x0\n" \
         ); \
     }
 #define WRAP_MED_FUNC(func) \
     void my_##func() { \
         asm volatile( \
-            "mov x27, x30\n" \
-            "ldp x6, x7, [x28]\n" \
+            "stp x28, x30, [sp, #-16]!\n" \
+            "ldp x6, x7, [x28], #16\n" \
             "bl " #func "\n" \
-            "mov x30, x27\n" \
+            "ldp x28, x30, [sp], #16\n" \
             "mov x9, x0\n" \
         ); \
     }
 #define WRAP_BIG_FUNC(func) \
     void my_##func() { \
         asm volatile( \
-            "mov x27, x30\n" \
+            "stp x28, x30, [sp, #-16]!\n" \
             "ldp x6, x7, [x28], #16\n" \
+            "mov x20, sp\n" \
             "mov sp, x28\n" \
             "bl " #func "\n" \
-            "sub x28, x28, #16\n" \
-            "mov x30, x27\n" \
+            "mov sp, x20\n" \
+            "ldp x28, x30, [sp], #16\n" \
             "mov x9, x0\n" \
         ); \
     }
