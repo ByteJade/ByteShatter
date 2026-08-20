a2dd05db9e48ee881a77ec38217efd85c4fc21f9
ByteJade <cuvakcernyj66@gmail.com>
2026-08-20 14:41:34 +0300

    fix stack

--- src/encoder.c
+++ src/encoder.c
@@ -380,7 +380,7 @@ void encode(Context* context, Instruction* buf) {
                 break;
             }
             if (prev_instruction == PUSH) {
-                emit_sub_signed(context, SC1R, 31, 8);
+                emit_sub_signed(context, SC1R, 31, 16);
                 emit32(context, STP | (prev_register<<10) | (SC1R<<5) | (x64_regs[r0]));
                 emit32(context, sf|ADD_IMM | 31 | (SC1R << 5));
                 prev_instruction = NOP;
