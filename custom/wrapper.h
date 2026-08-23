168a1802169f1211d5e63500f8870fbb2b183436
ByteJade <cuvakcernyj66@gmail.com>
2026-08-23 17:27:54 +0300

    wrapper update

--- custom/wrapper.h
+++ custom/wrapper.h
@@ -6,19 +6,19 @@
 #define WRAP_FUNC(func) \
     void my_##func() { \
         asm volatile( \
-            "mov x28, x30\n" \
+            "stp x29, x30, [sp, #-16]!\n" \
             "bl " #func "\n" \
-            "mov x30, x28\n" \
+            "ldp x29, x30, [sp], #16\n" \
             "mov x9, x0\n" \
         ); \
     }
 #define WRAP_MED_FUNC(func) \
     void my_##func() { \
         asm volatile( \
-            "mov x28, x30\n" \
+            "stp x29, x30, [sp, #-16]!\n" \
             "ldp x6, x7, [sp]\n" \
             "bl " #func "\n" \
-            "mov x30, x28\n" \
+            "ldp x29, x30, [sp], #16\n" \
             "mov x9, x0\n" \
         ); \
     }
